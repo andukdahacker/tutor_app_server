c514e269df8db345d5b4d212a1cec6ccc16c98a9
DucDA <ducda@omt.vn>
2023-04-28 17:26:56 +0700

    update

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { PubSubModule } from 'src/pub-sub/pub-sub.module';
 import { NotificationResolver } from './notification.resolver';
 import { NotificationService } from './notification.service';
 
 @Module({
   providers: [NotificationResolver, NotificationService],
-  imports: [PubSubModule],
 })
 export class NotificationModule {}
--- src/notification/notification.resolver.ts
+++ src/notification/notification.resolver.ts
@@ -1,8 +1,9 @@
 import { Inject } from '@nestjs/common';
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Subscription } from '@nestjs/graphql';
 import { RedisPubSub } from 'graphql-redis-subscriptions';
 
 import { PUB_SUB } from 'src/pub-sub/pub-sub.module';
+import { Notification } from './dto/entities';
 import { NotificationService } from './notification.service';
 
 @Resolver()
@@ -11,4 +12,9 @@ export class NotificationResolver {
     private readonly notificationService: NotificationService,
     @Inject(PUB_SUB) private pubSub: RedisPubSub,
   ) {}
+
+  @Subscription(() => Notification)
+  notifications() {
+    return this.pubSub.asyncIterator(['LEARNER_REQUEST']);
+  }
 }
