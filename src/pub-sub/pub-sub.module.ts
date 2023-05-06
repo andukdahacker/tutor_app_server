8ecb50f6b91dcadb089eb055da06588b8a176213
DucDA <ducda@omt.vn>
2023-05-06 11:03:15 +0700

    update

--- src/pub-sub/pub-sub.module.ts
+++ src/pub-sub/pub-sub.module.ts
@@ -2,6 +2,7 @@ import { Global, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { RedisPubSub } from 'graphql-redis-subscriptions';
 import Redis, { RedisOptions } from 'ioredis';
+import { Environment } from 'src/config/env.validation';
 
 export const PUB_SUB = 'PUB_SUB';
 
@@ -16,9 +17,17 @@ export const PUB_SUB = 'PUB_SUB';
           host: configService.get('REDIS_HOST'),
           port: configService.get('REDIS_PORT'),
         };
+
+        const environment = configService.get('NODE_ENV') as Environment;
         return new RedisPubSub({
-          subscriber: new Redis(redisOption),
-          publisher: new Redis(redisOption),
+          subscriber:
+            environment === Environment.Production
+              ? new Redis(process.env.REDIS_URL as string)
+              : new Redis(redisOption),
+          publisher:
+            environment === Environment.Production
+              ? new Redis(process.env.REDIS_URL as string)
+              : new Redis(redisOption),
         });
       },
       inject: [ConfigService],
--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import Redis from 'ioredis';
+import { Environment } from 'src/config/env.validation';
 
 export const REDIS = 'IORedis';
 
@@ -12,6 +13,10 @@ export const REDIS = 'IORedis';
       provide: REDIS,
       inject: [ConfigService],
       useFactory(configService: ConfigService) {
+        const enviroment = configService.get('NODE_ENV') as Environment;
+        if (enviroment === Environment.Production) {
+          return new Redis(process.env.REDIS_URL as string);
+        }
         return new Redis({
           host: configService.get('REDIS_HOST'),
           port: configService.get('REDIS_PORT'),
